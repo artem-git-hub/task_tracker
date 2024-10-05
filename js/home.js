afc922a278d61377395471fd7b71ad021187bc63
Александр Ларин <alex175alex@ro.ru>
2024-10-05 19:19:27 +0300

    Добавление модального окна создания проекта на домашней странице

--- js/home.js
+++ js/home.js
@@ -1,5 +1,6 @@
+// Смена надписи при нажатии на кнопку разворачивания
 function collapseExpandTextToggle(element) {
-    const toggleText = $(element).find('.toggleText');
+    let toggleText = $(element).find('.toggleText');
 
     if (toggleText.html().includes('Развернуть')) {
         toggleText.html('Свернуть <i class="bi bi-chevron-up"></i>');
@@ -7,3 +8,10 @@ function collapseExpandTextToggle(element) {
         toggleText.html('Развернуть <i class="bi bi-chevron-down"></i>');
     }
 }
+
+// Очистка данных в модальном окне проекта при открытии
+$('#modalProject').on('show.bs.modal', function (event) {
+    let modalElement = $(event.currentTarget);
+    modalElement.find('[name="blockTitle"]').val('');
+    modalElement.find('textarea').val('');
+});
