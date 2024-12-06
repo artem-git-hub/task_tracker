f46b7dd3a3b3c4b93b94c0f43d98e6382f8bc51f
Александр Ларин <alex175alex@ro.ru>
2024-12-06 21:32:50 +0300

    Добавление редактирования аватарка в профиле

--- js/profile.js
+++ js/profile.js
@@ -1,17 +1,60 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const saveButton = document.querySelector('#editModal .btn-primary');
+$(function () {
+    $('#editModal .btn-primary').on('click', () => {
+        let userName = $('#userName').val();
+        let userLogin = $('#userLogin').val();
+        let aboutText = $('#aboutText').val();
+        let imageFile = $('#editModal').find('input[type="file"]')[0].files[0];
 
-    saveButton.addEventListener('click', () => {
-        const userName = document.getElementById('userName').value;
-        const userLogin = document.getElementById('userLogin').value;
-        const aboutText = document.getElementById('aboutText').value;
+        $.ajax({
+            url: '/profile/update',
+            type: 'POST',
+            data: JSON.stringify({
+                name: userName,
+                login: userLogin,
+                about: aboutText,
+                image: imageFile,
+            }),
+            success: (response) => {
+                console.log('Данные успешно сохранены:', response);
+            },
+            error: function (error) {
+                console.error('Ошибка отправки данных:', error);
+            },
+        });
 
-        // Here you can add code to update the profile fields or make an API request to save them
-
-        // Close modal after saving
-        const modal = bootstrap.Modal.getInstance(
-            document.getElementById('editModal')
-        );
+        let modal = bootstrap.Modal.getInstance($('#editModal')[0]);
         modal.hide();
     });
+
+    $('#editModal').on('show.bs.modal', function (event) {
+        let modalElement = $(event.currentTarget);
+        let userName = $('#name').text();
+        let userLogin = $('#login').text();
+        let aboutText = $('#about').text();
+        let avatarSrc = $('.profile-avatar img').attr('src');
+
+        modalElement.find('input[type="file"]').val(null);
+
+        $('#userName').val(userName);
+        $('#userLogin').val(userLogin);
+        $('#aboutText').val(aboutText.trim());
+        $('#imagePreview').attr('src', avatarSrc);
+    });
 });
+
+function previewImage(event) {
+    const file = event.target.files[0];
+
+    if (file && file.type.match('image.*')) {
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#imagePreview').attr('src', e.target.result);
+        };
+
+        reader.readAsDataURL(file);
+    } else {
+        alert('Пожалуйста, выберите изображение.');
+        $(event.target).val(null);
+    }
+}
